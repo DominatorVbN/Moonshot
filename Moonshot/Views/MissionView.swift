d890f200f29d1d8cfc146c0802d0740c2c0150d4
Dominator <amitsamant@outlook.com>
2020-04-13 20:18:43 +0530

    Day 93 challenge 1 completed

--- Moonshot/Views/MissionView.swift
+++ Moonshot/Views/MissionView.swift
@@ -36,10 +36,19 @@ struct MissionView: View {
         GeometryReader{ geo in
             ScrollView{
                 VStack(alignment: .center){
-                    Image(self.mission.image)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(maxWidth: geo.size.width * 0.7)
+                    GeometryReader { innerGeo in
+                            HStack{
+                                Spacer()
+                                Image(self.mission.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxWidth: (geo.size.width * 0.7))
+                                    .scaleEffect(1-(((innerGeo.frame(in: .global).minY - geo.safeAreaInsets.top)/100) > 0.4 ? 0.4 : ((innerGeo.frame(in: .global).minY - geo.safeAreaInsets.top)/100) < -0.2 ? -0.2 : ((innerGeo.frame(in: .global).minY - geo.safeAreaInsets.top)/100)))
+                                Spacer()
+                            }
+
+                    }
+                        //innerGeo.frame(in: .glonal).minY/geo.size.height
                     Text("Launch date: ") + Text(self.mission.launchDate != nil ? "\(DateFormatter.localizedString(from: self.mission.launchDate!, dateStyle: DateFormatter.Style.long, timeStyle: DateFormatter.Style.none))" : "N/A")
                     Text(self.mission.description)
                         .padding()
@@ -59,7 +68,8 @@ struct MissionView: View {
                                     
                                 }
                                 Spacer()
-                            }                       .padding(.horizontal)
+                            }
+                            .padding(.horizontal)
                         }
                         .buttonStyle(PlainButtonStyle())
                     }
